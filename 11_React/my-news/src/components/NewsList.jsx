dafc3bd180a6bb6a0ba22d9f000df438be1ec69c
geoblo <geoblo@naver.com>
2024-06-03 15:18:42 +0900

    up

---
+++ 11_React/my-news/src/components/NewsList.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+function NewsList() {
+  return (
+    <>
+    </>
+  );
+};
+
+export default NewsList;
